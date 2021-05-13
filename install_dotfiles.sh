4b785f31fcaa357ba4627c6a20b2bb4401911371
pysan3 <pysan3@gmail.com>
2021-05-13 14:32:20 +0900

    create symlink to files inside config to $XDG_CONFIG_HOME

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -10,7 +10,7 @@ fi
 
 WORKDIR=$PWD
 cd $DIR
-for f in `command ls -ap | grep -v / | grep -v '.session.vim'`; do
+for f in `command ls -Ap | grep -v / | grep -v '.session.vim' | grep -v 'test' | grep -v 'tmp'`; do
     if [ -f "$HOME/$f" ]; then
         echo "$HOME/$f: Symbolic link already exists."
     else
@@ -22,15 +22,17 @@ if [ -z "$XDG_CONFIG_HOME" ]; then
     echo 'setting $XDG_CONFIG_HOME to '"$HOME/.config"
     XDG_CONFIG_HOME="$HOME/.config"
 fi
-for f in `command ls -ap config | grep -v /`; do
-    if [ ! -f "$XDG_CONFIG_HOME/$f" ]; then
-        ln -s "$DIR/$f" "$XDG_CONFIG_HOME/$f"
+for f in `command find "config" -type f`; do
+    file=${f#"config/"}
+    if [ ! -f "$XDG_CONFIG_HOME/$file" ]; then
+        mkdir -p `dirname "$XDG_CONFIG_HOME/$file"`
+        ln -s "$DIR/$f" "$XDG_CONFIG_HOME/$file"
     fi
 done
 if [ -d ./nvim ]; then
     mkdir -p "$XDG_CONFIG_HOME/nvim/session"
     mkdir -p "$XDG_CONFIG_HOME/nvim/undodir"
-    cp -rs "$DIR/nvim" "$XDG_CONFIG_HOME"
+    cp -rs "$DIR/nvim" "$XDG_CONFIG_HOME" 2>/dev/null
     if ! command -v nvim &> /dev/null; then
         echo -n 'It seems neovim is not installed. Install? '
         result=0
