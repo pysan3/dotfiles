43c982c04070c7c2cd81a06d50f7db140ffd44c9
pysan3 <pysan3@gmail.com>
2021-06-03 07:11:01 +0900

    update apt, pacman, pip packages

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# update all globally installed pip packages
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
+if [[ x`basename $SHELL` = x'zsh' ]]; then
+    :
+else
+    :
+fi
+
+if ! command -v 'yay' &> /dev/null; then
+    yay -Syu
+elif ! command -v 'apt' &> /dev/null; then
+    sudo apt update && sudo apt upgrade -y
+fi
+
