727de391c1f1a1293754a7c061f564d32d556f7a
pysan3 <pysan3@gmail.com>
2021-06-14 13:52:20 +0900

    update all global npm packages

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 # update all globally installed pip packages
 pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 
+# update global npm packages
+npm update -g
+
 if [[ x`basename $SHELL` = x'zsh' ]]; then
     :
 else
