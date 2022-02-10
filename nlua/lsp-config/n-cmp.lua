c0feab30fffc4fc030459f52d48d85a0476bfd41
pysan3 <pysan3@gmail.com>
2022-02-10 16:18:32 +0900

    small bug fix

--- nlua/lsp-config/n-cmp.lua
+++ nlua/lsp-config/n-cmp.lua
@@ -1,7 +1,7 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 local check_backspace = function()
   local col = vim.fn.col(".") - 1
