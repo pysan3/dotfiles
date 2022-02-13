236453baaa5524899d87e1aaac7fc699e116c359
pysan3 <pysan3@gmail.com>
2022-02-14 01:10:40 +0900

    install nvim, nvim/lua configs with posix commands

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -9,12 +9,12 @@ if [[ 'xdotfiles' != x$(basename $DOTFILES) ]]; then
 fi
 
 WORKDIR=$PWD
-cd $DOTFILES
+cd $DOTFILES || error "Could not cd to $DOTFILES; Abort" || exit
 
 source "$DOTFILES/functions.zsh"
 
 # create symlink to .zsh* files
-for f in `command ls -Ap | grep -v / | grep -v '\.sh' | grep -v '\.zsh$'`; do
+for f in $(command ls -Ap | grep -v / | grep -v '\.sh' | grep -v '\.zsh$'); do
     if [[ "$f" =~ (\.git|\.session|test|tmp|local|list|README|LICENSE).* ]]; then continue; fi
     if [ -f "$HOME/$f" ]; then
         warning "$HOME/$f: Symbolic link already exists."
@@ -29,10 +29,10 @@ if [ -z "$XDG_CONFIG_HOME" ]; then
     XDG_CONFIG_HOME="$HOME/.config"
 fi
 info "$(tput setaf 4)XDG_CONFIG_HOME$(tput sgr0) := $HOME/.config"
-for f in `command find "config" -type f`; do
+for f in $(command find "config" -type f); do
     file=${f#"config/"}
     if [ ! -f "$XDG_CONFIG_HOME/$file" ]; then
-        dir_name=`dirname "$XDG_CONFIG_HOME/$file"`
+        dir_name=$(dirname "$XDG_CONFIG_HOME/$file")
         mkdir -p "$dir_name"
         warning "Created dir: $dir_name"
         ln -s "$DOTFILES/$f" "$XDG_CONFIG_HOME/$file"
@@ -44,6 +44,17 @@ for f in `command find "config" -type f`; do
     fi
 done
 
+# neovim configs and install extensions
+function link_nvim () {
+    NFROM="$1"; NTO="$2"; NMSG="$3 to $2"
+    [ ! -d "$NFROM" ] && error "$NFROM not found." && return 1
+    if [[ -d "$NTO" && x$(followlink "$NTO") = x"$NFROM" ]]; then info "$NMSG already installed"
+    else ln -s "$NFROM" "$NTO" && info "Installed $NMSG" || error "Failed to install $NMSG"
+    fi
+}
+link_nvim "$DOTFILES"/nvim "$XDG_CONFIG_HOME"/nvim "nvim config files"
+link_nvim "$DOTFILES"/nlua "$XDG_CONFIG_HOME"/nvim/lua "nlua config files"
+
 # install files in ./static/
 if [ ! -f ~/texmf/tex/latex/local/pdfpc-commands.sty ]; then
     mkdir -p ~/texmf/tex/latex/local
@@ -54,15 +65,6 @@ if [ ! -f ~/texmf/tex/latex/local/pdfpc-commands.sty ]; then
     info "Installed pdfpc-commands.sty"
 fi
 
-# neovim configs and install extensions
-if [ -d "$DOTFILES/nvim" ]; then
-    ln -s "$DOTFILES"/nvim "$XDG_CONFIG_HOME"/nvim
-    info "Installed nvim config files to $XDG_CONFIG_HOME/nvim"
-fi
-if [ -d "$DOTFILES/nlua" ]; then
-    ln -s "$DOTFILES"/nlua "$XDG_CONFIG_HOME"/nvim/lua
-    info "Installed nlua config files to $XDG_CONFIG_HOME/nvim/lua"
-fi
-cd $WORKDIR
+cd $WORKDIR || error "Could not cd to $WORKDIR; Abort" || exit
 
 checkdependency git
