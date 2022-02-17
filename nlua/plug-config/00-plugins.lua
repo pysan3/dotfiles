a25166d3c386d4eb685ce35135453d12dde235fa
pysan3 <pysan3@gmail.com>
2022-02-18 04:36:29 +0900

    fixed typo

--- nlua/plug-config/00-plugins.lua
+++ nlua/plug-config/00-plugins.lua
@@ -34,7 +34,7 @@ local packer = require("packer")
 return packer.startup(function(use)
   use("wbthomason/packer.nvim") -- Have packer manage itself
   use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-  use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+  use("nvim-lua/plenary.nvim") -- Useful lua functions used lots of plugins
 
   -- git
   use("lewis6991/gitsigns.nvim")
