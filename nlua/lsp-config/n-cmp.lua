cb2f9669afbb99fe0f882e1a0aada279f45cfe92
pysan3 <pysan3@gmail.com>
2022-02-23 16:15:10 +0900

    install system dictionaries to cmp words if exist

--- nlua/lsp-config/n-cmp.lua
+++ nlua/lsp-config/n-cmp.lua
@@ -96,14 +96,21 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
 -- from cmp-dictionary
-local dict_source = {} -- sudo apt install wamerican
-
-if vim.fn.filereadable(vim.fn.expand("/usr/share/dict/words")) ~= 0 then
-  table.insert(dict_source, "/usr/share/dict/words")
-end
+local dict_source = {}
 -- add my spell lists; $XDG_CONFIG_HOME/nvim/spell/*.add
 for filepath in string.gmatch(vim.fn.glob(vim.env.XDG_CONFIG_HOME .. "/nvim/spell/*.add"), "[^\n]+") do
   table.insert(dict_source, filepath)
+end
+-- add system installed dictionaries
+local share_dict_source = {
+  "wamerican",
+  "wamerican-insane",
+  "wngerman",
+}
+for _, source in ipairs(share_dict_source) do
+  if vim.fn.filereadable(vim.fn.expand("/usr/share/dict/" .. source)) ~= 0 then
+    table.insert(dict_source, "/usr/share/dict/" .. source)
+  end
 end
 
 require("cmp_dictionary").setup({
