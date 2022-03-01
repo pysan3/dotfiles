0e5d1ee2f29baf6c9b55f8fad171754fa31022ac
pysan3 <pysan3@gmail.com>
2022-03-02 05:47:19 +0900

    export setup function for each lsp config file

--- nlua/lsp-config/n-lsp-base.lua
+++ nlua/lsp-config/n-lsp-base.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.setup = function()
+M.setup = function(_)
   local signs = {
     { name = "DiagnosticSignError", text = "" },
     { name = "DiagnosticSignWarn", text = "" },
@@ -76,7 +76,6 @@ M.lsp_keymaps = function(bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 return M
--- nlua/lsp-config/n-lsp-null.lua
+++ nlua/lsp-config/n-lsp-null.lua
@@ -5,32 +5,38 @@ local fmt = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diag = null_ls.builtins.diagnostics
 
-null_ls.setup({
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd([[
+local M = {}
+
+M.setup = function(_)
+  null_ls.setup({
+    on_attach = function(client)
+      if client.resolved_capabilities.document_formatting then
+        vim.cmd([[
         augroup LspFormatting
         autocmd! * <buffer>
         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 5000)
         augroup END
       ]])
-    end
-  end,
-  debug = false,
-  sources = {
-    -- js, ts
-    fmt.prettier,
-    diag.eslint,
-    -- python
-    fmt.autopep8.with({ extra_args = { "--max-line-length=120", "--aggressive", "--aggressive" } }),
-    diag.flake8.with({ extra_args = { "--max-line-length=120", "--ignore=F405" } }),
-    -- lua
-    fmt.stylua.with({
-      extra_args = {
-        "--indent-width=2",
-        "--indent-type=Spaces",
-        "--quote-style=AutoPreferDouble",
-      },
-    }),
-  },
-})
+      end
+    end,
+    debug = false,
+    sources = {
+      -- js, ts
+      fmt.prettier,
+      diag.eslint,
+      -- python
+      fmt.autopep8.with({ extra_args = { "--max-line-length=120", "--aggressive", "--aggressive" } }),
+      diag.flake8.with({ extra_args = { "--max-line-length=120", "--ignore=F405" } }),
+      -- lua
+      fmt.stylua.with({
+        extra_args = {
+          "--indent-width=2",
+          "--indent-type=Spaces",
+          "--quote-style=AutoPreferDouble",
+        },
+      }),
+    },
+  })
+end
+
+return M
