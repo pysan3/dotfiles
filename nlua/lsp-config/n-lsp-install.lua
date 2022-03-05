82a40dee379affb2aafb5cb549c94cdf3a93a0a5
pysan3 <pysan3@gmail.com>
2022-03-05 23:05:50 +0900

    optimize lsp installation at startup

--- nlua/lsp-config/n-lsp-install.lua
+++ nlua/lsp-config/n-lsp-install.lua
@@ -114,30 +114,26 @@ local stop_lsp_fmt = {
   pylsp = 1,
 }
 
+local global_opts = {
+  capabilities = lsp_base.capabilities,
+  on_attach = function(client, bufnr)
+    if stop_lsp_fmt[client.name] ~= nil then
+      client.resolved_capabilities.document_formatting = false
+    end
+    lsp_base.lsp_keymaps(bufnr)
+    lsp_base.lsp_highlight_document(client)
+  end,
+}
+
 local M = {}
 
 M.setup = function(_)
   for server_name, server_opt in pairs(servers) do
-    local opts = {
-      capabilities = lsp_base.capabilities,
-      on_attach = function(client, bufnr)
-        if stop_lsp_fmt[client.name] ~= nil then
-          client.resolved_capabilities.document_formatting = false
-        end
-        lsp_base.lsp_keymaps(bufnr)
-        lsp_base.lsp_highlight_document(client)
-      end,
-    }
     local server_ok, server = lsp_installer.get_server(server_name)
+    local is_opt, file_opt = pcall(require, "lsp-config.settings." .. server_name)
+    local opts = vim.tbl_deep_extend("force", is_opt and file_opt or {}, server_opt or {}, global_opts)
     if server_ok then
       server:on_ready(function()
-        local is_opt, file_opt = pcall(require, "lsp-config.settings." .. server_name)
-        if is_opt then
-          opts = vim.tbl_deep_extend("force", file_opt, opts)
-        end
-        if server_opt ~= nil then
-          opts = vim.tbl_deep_extend("force", server_opt, opts)
-        end
         server:setup(opts)
       end)
       if not server:is_installed() then
