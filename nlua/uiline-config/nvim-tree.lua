3f777cc3f1a290673ebd1195d081a7f2e7e6640b
pysan3 <pysan3@gmail.com>
2022-04-01 10:25:26 +0900

    delete deprecated config in nvim-tree

--- nlua/uiline-config/nvim-tree.lua
+++ nlua/uiline-config/nvim-tree.lua
@@ -14,7 +14,6 @@ require("nvim-tree").setup({
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = {},
-  auto_close = true,
   auto_reload_on_write = true,
   open_on_tab = false,
   hijack_cursor = true,
