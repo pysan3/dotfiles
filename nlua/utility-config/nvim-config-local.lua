2156194987a988dbacfb868c8faf2f30b878e2c4
pysan3 <pysan3@gmail.com>
2022-05-23 19:10:39 +0900

    merge configs for nvim 0.7, 0.8

--- nlua/utility-config/nvim-config-local.lua
+++ nlua/utility-config/nvim-config-local.lua
@@ -1,5 +1,10 @@
 require("config-local").setup({
-  config_files = { "./.vim/local.vim", "./.vim/local.lua" }, -- Config file patterns to load (lua supported)
+  config_files = { -- Config file patterns to load (lua supported)
+    ".vim/local.vim",
+    "./.vim/local.vim",
+    ".vim/local.lua",
+    "./.vim/local.lua",
+  },
   hashfile = vim.fn.stdpath("data") .. "/config_local", -- Where the plugin keeps files data
   autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
   commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
