b026cecd0a16fbd4eae8d2d0effe6beb3bbb4705
pysan3 <pysan3@gmail.com>
2022-06-01 17:31:41 +0900

    better plugin setup so that themes will be required

--- nlua/uiline-config/init.lua
+++ nlua/uiline-config/init.lua
@@ -3,17 +3,6 @@ return {
     "norcalli/nvim-colorizer.lua",
     {
       "kyazdani42/nvim-web-devicons",
-      requires = {
-        "lunarvim/darkplus.nvim",
-        "christianchiarulli/nvcode-color-schemes.vim",
-        "EdenEast/nightfox.nvim",
-        "joshdick/onedark.vim",
-        "ulwlu/elly.vim",
-        "tomasiser/vim-code-dark",
-        "arcticicestudio/nord-vim",
-        "chriskempson/vim-tomorrow-theme",
-        "doki-theme/doki-theme-vim",
-      },
       setup = function()
         return vim.fn.has("termguicolors") == 1 and vim.cmd("set termguicolors")
       end,
@@ -24,5 +13,14 @@ return {
   },
   install = {
     "Yggdroot/indentLine", -- show indent line with |
+    "lunarvim/darkplus.nvim",
+    "christianchiarulli/nvcode-color-schemes.vim",
+    "EdenEast/nightfox.nvim",
+    "joshdick/onedark.vim",
+    "ulwlu/elly.vim",
+    "tomasiser/vim-code-dark",
+    "arcticicestudio/nord-vim",
+    "chriskempson/vim-tomorrow-theme",
+    "doki-theme/doki-theme-vim",
   },
 }
