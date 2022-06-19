bdf5962779bc7453c3d7c3d3b41706c87922e23b
pysan3 <pysan3@gmail.com>
2022-06-19 15:42:09 +0900

    add more builtin functions in telescope

--- nlua/utility-config/telescope.lua
+++ nlua/utility-config/telescope.lua
@@ -61,11 +61,23 @@ telescope.setup({
 telescope.load_extension("fzf")
 telescope.load_extension("media_files")
 
-vim.cmd([[
-nnoremap <leader>fp <cmd>Telescope git_files<CR>
-nnoremap <leader>fl <cmd>Telescope find_files<CR>
-nnoremap <leader>fs <cmd>Telescope live_grep<CR>
-nnoremap <leader>fh <cmd>Telescope help_tags<CR>
-nnoremap <leader>fr <cmd>Telescope resume<CR>
-nnoremap <leader>fi <cmd>Telescope media_files<CR>
-]])
+local ts_builtin = require("telescope.builtin")
+local ts_prefix = "<leader>f"
+
+-- telescope fzf bindings
+vim.keymap.set("n", ts_prefix .. "p", ts_builtin.git_files)
+vim.keymap.set("n", ts_prefix .. "l", ts_builtin.find_files)
+vim.keymap.set("n", ts_prefix .. "s", ts_builtin.live_grep)
+vim.keymap.set("n", ts_prefix .. "h", ts_builtin.help_tags)
+vim.keymap.set("n", ts_prefix .. "r", ts_builtin.resume)
+vim.keymap.set("n", ts_prefix .. "j", ts_builtin.jumplist)
+vim.keymap.set("n", ts_prefix .. "i", telescope.extensions.media_files.media_files)
+
+-- telescope lsp bindings
+vim.keymap.set("n", ts_prefix .. "t", ts_builtin.lsp_document_symbols)
+vim.keymap.set("n", ts_prefix .. "y", ts_builtin.lsp_workspace_symbols)
+
+-- telescope git bindings
+vim.keymap.set("n", ts_prefix .. "gc", ts_builtin.git_commits)
+vim.keymap.set("n", ts_prefix .. "gl", ts_builtin.git_bcommits)
+vim.keymap.set("n", ts_prefix .. "gb", ts_builtin.git_branches)
