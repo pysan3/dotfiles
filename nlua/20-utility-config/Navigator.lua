681bc20ae9841a74ede9a497181024220f2aba9a
pysan3 <pysan3@gmail.com>
2022-07-05 17:24:31 +0900

    execute Navigator commands regardless of the mode

--- nlua/20-utility-config/Navigator.lua
+++ nlua/20-utility-config/Navigator.lua
@@ -1,8 +1,9 @@
 require("Navigator").setup()
 
 -- Keybindings
-vim.keymap.set({ "n", "v", "i" }, "<A-h>", "<CMD>NavigatorLeft<CR>")
-vim.keymap.set({ "n", "v", "i" }, "<A-l>", "<CMD>NavigatorRight<CR>")
-vim.keymap.set({ "n", "v", "i" }, "<A-k>", "<CMD>NavigatorUp<CR>")
-vim.keymap.set({ "n", "v", "i" }, "<A-j>", "<CMD>NavigatorDown<CR>")
--- vim.keymap.set({ "n", "v", "i" }, "<A-p>", "<CMD>NavigatorPrevious<CR>")
+local all_modes = { "n", "v", "o", "i", "c", "t" }
+vim.keymap.set(all_modes, "<A-h>", "<CMD>NavigatorLeft<CR>")
+vim.keymap.set(all_modes, "<A-l>", "<CMD>NavigatorRight<CR>")
+vim.keymap.set(all_modes, "<A-k>", "<CMD>NavigatorUp<CR>")
+vim.keymap.set(all_modes, "<A-j>", "<CMD>NavigatorDown<CR>")
+vim.keymap.set(all_modes, "<A-p>", "<CMD>NavigatorPrevious<CR>")
