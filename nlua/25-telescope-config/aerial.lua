73e97e886f44088e2ad0d31d0fa52f0d9023a64c
pysan3 <pysan3@gmail.com>
2022-07-07 14:08:36 +0900

    change aerial keybinds to avoid conflicts

--- nlua/25-telescope-config/aerial.lua
+++ nlua/25-telescope-config/aerial.lua
@@ -20,8 +20,8 @@ require("aerial").setup({
     -- Toggle the aerial window with <leader>a
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
     -- Jump forwards/backwards with '{' and '}'
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "[a", "<cmd>AerialPrev<CR>", {})
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "]a", "<cmd>AerialNext<CR>", {})
     -- Jump up the tree with '[[' or ']]'
     vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
     vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
