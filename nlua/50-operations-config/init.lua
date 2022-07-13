066f680cfa6229254ac09942d0538935aa7d82d9
pysan3 <pysan3@gmail.com>
2022-07-13 18:12:49 +0900

    migrate from tpope/vim-surround to nvim-surround

--- nlua/50-operations-config/init.lua
+++ nlua/50-operations-config/init.lua
@@ -4,14 +4,13 @@ return {
     "unblevable/quick-scope", -- highlights f, t, F, T
     -- "justinmk/vim-sneak", -- s, S to jump anywhere
     "phaazon/hop.nvim",
+    "kylechui/nvim-surround", -- s is motion, ys to add
   },
   install = {
     "tpope/vim-repeat", -- better repetition
     "christoomey/vim-titlecase", -- gzz
     "christoomey/vim-sort-motion", -- gs<motion> eg. gs2j => sort 3 lines
-    "tpope/vim-surround", -- s is motion, ys to add
-    -- "michaeljsmith/vim-indent-object", -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
-    -- "PeterRincker/vim-argumentative",
+    "michaeljsmith/vim-indent-object", -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
     "wellle/targets.vim", -- 2i, 2a for more inner and outer selection
   },
 }
