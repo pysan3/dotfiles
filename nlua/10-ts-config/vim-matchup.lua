7e28217ec5c14ae85b988c2cdacfea4640ddfdff
pysan3 <pysan3@gmail.com>
2022-07-14 23:02:09 +0900

    disable showing offscreen matchups in statusline

--- nlua/10-ts-config/vim-matchup.lua
+++ nlua/10-ts-config/vim-matchup.lua
@@ -1,3 +1,4 @@
 vim.g.matchup_surround_enabled = 1
 vim.g.matchup_delim_noskips = 1 -- recognize symbols within comments
 vim.g.matchup_matchparen_enabled = 1
+vim.g.matchup_matchparen_offscreen = {}
