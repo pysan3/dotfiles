a00d9ec42d7406ce3350d183fa9020a66ac6cfd4
pysan3 <pysan3@gmail.com>
2022-07-28 11:27:38 +0900

    update submodules of nvim

--- install_base.zsh
+++ install_base.zsh
@@ -347,6 +347,7 @@ fi
 if ! command -v 'nvim' &>/dev/null || $NVIM_UPDATE_ALL || checkyes 'Install nvim from source?'; then
   NVIM_INSTLL_DIR="$XDG_DATA_HOME/nvim-git"
   update_git_repo "$NVIM_INSTLL_DIR" https://github.com/neovim/neovim.git "${NVIM_BUILD_TAG:-stable}"
+  git -C "$NVIM_INSTLL_DIR" submodule update --init --recursive
   cd "$NVIM_INSTLL_DIR"
   make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$XDG_PREFIX_HOME" install || error 'NVIM BUILD FAILED'
   info 'nvim installed'
