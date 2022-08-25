dcc0ef4daaa6041fe206108828fbc69c1b5f8cd5
pysan3 <pysan3@gmail.com>
2022-08-25 12:34:29 +0900

    update Comment.nvim config to add correct comment strings

--- nlua/70-cmp-config/Comment.lua
+++ nlua/70-cmp-config/Comment.lua
@@ -6,19 +6,19 @@ comment.setup({
     extended = true,
   },
   pre_hook = function(ctx)
-    local U = require("Comment.utils")
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
+    -- Only calculate commentstring for tsx filetypes
+    if vim.bo.filetype == "typescriptreact" then
+      local U, location = require("Comment.utils"), nil
+      if ctx.ctype == U.ctype.blockwise then
+        location = require("ts_context_commentstring.utils").get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
+      end
+      return require("ts_context_commentstring.internal").calculate_commentstring({
+        key = ctx.ctype == U.ctype.linewise and "__default" or "__multiline",
+        location = location,
+      })
     end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring({
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    })
   end,
 })
 
