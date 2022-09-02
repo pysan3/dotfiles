fb3f30b0a1b002d5c1ef1c431c219b9d4f4a051c
pysan3 <pysan3@gmail.com>
2022-09-02 17:19:56 +0900

    use hop2 as default

--- nlua/50-operations-config/hop.lua
+++ nlua/50-operations-config/hop.lua
@@ -7,5 +7,5 @@ require("hop").setup({
 
 vim.keymap.set({ "n", "v" }, "<leader>jw", "<Cmd>HopWord<CR>", { silent = true, desc = "<Cmd>HopWord<CR>" })
 vim.keymap.set({ "n", "v" }, "<leader>jl", "<Cmd>HopLine<CR>", { silent = true, desc = "<Cmd>HopLine<CR>" })
-vim.keymap.set({ "n", "v" }, "s", "<Cmd>HopChar1<CR>", { silent = true, desc = "<Cmd>HopChar1<CR>" })
-vim.keymap.set({ "n", "v" }, "S", "<Cmd>HopChar2<CR>", { silent = true, desc = "<Cmd>HopChar2<CR>" })
+vim.keymap.set({ "n", "v" }, "S", "<Cmd>HopChar1<CR>", { silent = true, desc = "<Cmd>HopChar1<CR>" })
+vim.keymap.set({ "n", "v" }, "s", "<Cmd>HopChar2<CR>", { silent = true, desc = "<Cmd>HopChar2<CR>" })
