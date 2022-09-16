e3e6ad60cef031f52aa5be47c3f81035ee84ec35
pysan3 <pysan3@gmail.com>
2022-09-16 10:48:25 +0900

    fix null-ls temporary bug https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1103

--- nlua/70-lsp-config/init.lua
+++ nlua/70-lsp-config/init.lua
@@ -7,13 +7,16 @@ return {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "neovim/nvim-lspconfig", -- enable LSP
-        "jose-elias-alvarez/null-ls.nvim", -- linter
+        {
+          "jose-elias-alvarez/null-ls.nvim", -- linter
+          commit = "76d0573fc159839a9c4e62a0ac4f1046845cdd50",
+        },
         { "glepnir/lspsaga.nvim", branch = "main" },
         "andrewferrier/textobj-diagnostic.nvim",
       },
     },
     "ray-x/lsp_signature.nvim", -- show hints when writing function arguments
     "j-hui/fidget.nvim", -- print linting progress
-    {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
+    { "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" },
   },
 }
