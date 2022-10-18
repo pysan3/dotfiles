709cb1c7664a9fa871aeff217d409961680910b9
pysan3 <pysan3@gmail.com>
2022-10-18 12:09:27 +0900

    automatically activate conda environments

--- config/zsh/aliases_rc.zsh
+++ config/zsh/aliases_rc.zsh
@@ -97,6 +97,8 @@ alias piplist="pip freeze | grep -v 'pkg-resources' > requirements.txt; cat requ
 function act() {
   [ -f 'bin/activate' ] && source bin/activate
   [ -f '.venv/bin/activate' ] && source .venv/bin/activate
+  [ -f 'environment.yml' ] && conda activate $(cat environment.yml | grep name: | head -n 1 | cut -f 2 -d ':')
+  [ -f 'environment.yaml' ] && conda activate $(cat environment.yaml | grep name: | head -n 1 | cut -f 2 -d ':')
   return 0
 }
 act
