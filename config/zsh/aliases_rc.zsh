7ed21488f11ab18d324dd0fdd8bb5942dcb482fe
pysan3 <pysan3@gmail.com>
2022-11-24 17:26:10 +0900

    update appearance test function

--- config/zsh/aliases_rc.zsh
+++ config/zsh/aliases_rc.zsh
@@ -254,10 +254,26 @@ function pdfcompress () {
   	ps2pdf -dPDFSETTINGS=/prepress -dCompatibilityLevel=1.4 -sOutputFile="compressed-$1" "$1"
 }
 
+function colortest () {
+  T='gYw'   # The test text
+  echo 'Color Test'
+  echo '‾‾‾‾‾‾‾‾‾‾'
+  echo -e "                 40m     41m     42m     43m     44m     45m     46m     47m"
+  for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+             '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+             '  36m' '1;36m' '  37m' '1;37m'; do
+    FG=${FGs// /}
+    echo -en " $FGs \033[$FG  $T  "
+    for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m"; done
+    echo;
+  done
+}
+
 function appearance () {
   cat \
-    <(wget -O- 'https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-demo.txt' 2> /dev/null) \
+    <(colortest) \
     <(wget -O- 'https://unicode.org/Public/emoji/13.0/emoji-test.txt' 2> /dev/null) \
+    <(wget -O- 'https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-demo.txt' 2> /dev/null) \
     | less
 }
 
