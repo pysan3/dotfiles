a1c9d007d341c30091fbb8d4d2f7d3fc4db09ca4
pysan3 <pysan3@gmail.com>
2022-12-02 20:40:02 +0900

    longer visible time for notify

--- nlua/20-utility-config/nvim-notify.lua
+++ nlua/20-utility-config/nvim-notify.lua
@@ -1,7 +1,7 @@
 require("notify").setup({
   stages = "static", -- fade_in_slide_out, fade, slide, static
   render = "default",
-  timeout = 800,
+  timeout = 1200,
   background_colour = "Normal",
   minimum_width = 50,
   icons = { ERROR = "", WARN = "", INFO = "", DEBUG = "", TRACE = "✎" },
