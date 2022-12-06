6b1a943324b01187b78b595598d91c1e84b9276f
pysan3 <pysan3@gmail.com>
2022-12-06 12:34:52 +0900

    better output for fidget

--- nlua/70-lsp-config/fidget.lua
+++ nlua/70-lsp-config/fidget.lua
@@ -2,9 +2,9 @@ require("fidget").setup({
   text = {
     -- spinner list: https://github.com/j-hui/fidget.nvim/blob/main/lua/fidget/spinners.lua
     spinner = "dots_pulse", -- dots, line, dots_bounce, ...
-    done = "",
-    commenced = "",
-    completed = "",
+    done = " ",
+    commenced = " ",
+    completed = " ",
   },
   timer = {
     fidget_decay = 500,
