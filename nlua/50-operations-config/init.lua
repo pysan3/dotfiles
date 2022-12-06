df8c1f857024549114d87bd19b1403af7b255b7e
pysan3 <pysan3@gmail.com>
2022-12-07 01:06:03 +0900

    no lazy loading plugins for operations config

--- nlua/50-operations-config/init.lua
+++ nlua/50-operations-config/init.lua
@@ -1,14 +1,10 @@
 return {
-  setup = {
-    "terryma/vim-expand-region", -- + to expand, _ to shrink
-    "kylechui/nvim-surround", -- s is motion, ys to add
-    "ggandor/leap.nvim", -- s, S to jump anywhere
-  },
-  install = {
-    "tpope/vim-repeat", -- better repetition
-    "christoomey/vim-titlecase", -- gzz
-    "christoomey/vim-sort-motion", -- gs<motion> eg. gs2j => sort 3 lines
-    "michaeljsmith/vim-indent-object", -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
-    "wellle/targets.vim", -- 2i, 2a for more inner and outer selection
-  },
+  { "terryma/vim-expand-region" }, -- + to expand, _ to shrink
+  { "kylechui/nvim-surround" }, -- s is motion, ys to add
+  { "ggandor/leap.nvim" }, -- s, S to jump anywhere
+  { "tpope/vim-repeat" }, -- better repetition
+  { "christoomey/vim-titlecase" }, -- gzz
+  { "christoomey/vim-sort-motion" }, -- gs<motion> eg. gs2j => sort 3 lines
+  { "michaeljsmith/vim-indent-object" }, -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
+  { "wellle/targets.vim" }, -- 2i, 2a for more inner and outer selection
 }
