80d74dbe831707feddd692a76257f78c927e9360
pysan3 <pysan3@gmail.com>
2022-12-07 16:22:01 +0900

    lazy load operations

--- nlua/50-operations-config/init.lua
+++ nlua/50-operations-config/init.lua
@@ -1,13 +1,18 @@
+local function be(plugin)
+  plugin.event = { "BufRead", "BufNewFile", "BufEnter" }
+  return plugin
+end
+
 return {
   {
     "terryma/vim-expand-region", -- + to expand, _ to shrink
     keys = { "<Plug>(expand_region_expand)", "<Plug>(expand_region_shrink)" },
   },
-  { "kylechui/nvim-surround" }, -- s is motion, ys to add
-  { "ggandor/leap.nvim" }, -- s, S to jump anywhere
-  { "tpope/vim-repeat" }, -- better repetition
-  { "christoomey/vim-titlecase" }, -- gzz
-  { "christoomey/vim-sort-motion" }, -- gs<motion> eg. gs2j => sort 3 lines
-  { "michaeljsmith/vim-indent-object" }, -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
-  { "wellle/targets.vim" }, -- 2i, 2a for more inner and outer selection
+  be({ "kylechui/nvim-surround" }), -- s is motion, ys to add
+  be({ "ggandor/leap.nvim" }), -- s, S to jump anywhere
+  be({ "tpope/vim-repeat" }), -- better repetition
+  be({ "christoomey/vim-titlecase", keys = { "gzz" } }), -- gzz
+  be({ "christoomey/vim-sort-motion" }), -- gs<motion> eg. gs2j => sort 3 lines
+  be({ "michaeljsmith/vim-indent-object" }), -- ai, ii, aI, iI: object of same indent (i; above, a; above and below)
+  be({ "wellle/targets.vim" }), -- 2i, 2a for more inner and outer selection
 }
