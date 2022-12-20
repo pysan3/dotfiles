11fadda9560b1e76fbb41539e207353873d99cca
pysan3 <pysan3@gmail.com>
2022-12-20 23:18:02 +0900

    add kanagawa colorscheme

--- nlua/10-color-config/init.lua
+++ nlua/10-color-config/init.lua
@@ -1,14 +1,23 @@
 local function set_colorscheme(extends)
   return function()
-    vim.cmd(string.format("colorscheme %s", vim.g.personal_options.colorscheme))
     if extends ~= nil then
       extends()
     end
+    vim.cmd(string.format("colorscheme %s", vim.g.personal_options.colorscheme))
   end
 end
 
 return {
   install = {
+    {
+      "rebelot/kanagawa.nvim",
+      cond = vim.g.personal_options.colorscheme == "kanagawa",
+      config = set_colorscheme(function()
+        require("kanagawa").setup({
+          globalStatus = true,
+        })
+      end),
+    },
     {
       "christianchiarulli/nvcode-color-schemes.vim",
       cond = vim.g.personal_options.colorscheme == "nvcode",
