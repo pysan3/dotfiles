3b2b1f0c7dc35df62f2468a922ac4d35007d0866
pysan3 <pysan3@gmail.com>
2022-12-26 21:27:43 +0900

    delete distant.lua

--- nlua/50-command-config/distant.lua
+++
@@ -1,8 +0,0 @@
-require("distant").setup({
-  -- Applies Chip's personal settings to every machine you connect to
-  --
-  -- 1. Ensures that distant servers terminate with no connections
-  -- 2. Provides navigation bindings for remote directories
-  -- 3. Provides keybinding to jump into a remote file's parent directory
-  ["*"] = require("distant.settings").chip_default(),
-})
