312871e26eb979ec77b8de0887656983e19d3937
pysan3 <pysan3@gmail.com>
2022-12-31 16:14:08 +0900

    fix vim-matchup config

--- lua-plug/plugins/10-vim-matchup.lua
+++ lua-plug/plugins/10-vim-matchup.lua
@@ -1,10 +1,10 @@
 return {
   "andymass/vim-matchup",
   event = "BufReadPost",
-  config = function()
+  init = function()
     vim.g.matchup_surround_enabled = 1
     vim.g.matchup_delim_noskips = 1 -- recognize symbols within comments
     vim.g.matchup_matchparen_enabled = 1
-    vim.g.matchup_matchparen_offscreen = {}
+    vim.g.matchup_matchparen_offscreen = { method = "popup" }
   end,
 }
