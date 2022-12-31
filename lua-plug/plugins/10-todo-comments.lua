8e806cc0f0bcae9eeb98eac98d7a9b31e678be02
pysan3 <pysan3@gmail.com>
2022-12-31 15:48:12 +0900

    lazy install nvim-treesitter plugins

---
+++ lua-plug/plugins/10-todo-comments.lua
@@ -0,0 +1,55 @@
+return {
+  "folke/todo-comments.nvim",
+  cmd = { "TodoTrouble", "TodoTelescope" },
+  event = "BufReadPost",
+  keys = {
+    { "]t", function()
+      require("todo-comments").jump_next()
+    end, desc = "Next todo comment" },
+    { "[t", function()
+      require("todo-comments").jump_prev()
+    end, desc = "Previous todo comment" },
+  },
+  config = {
+    signs = true,
+    sign_priority = 8,
+    keywords = { -- keywords recognized as todo comments
+      FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+      TODO = { icon = " ", color = "info" },
+      HACK = { icon = " ", color = "warning" },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO", "HINT" } },
+    },
+    merge_keywords = true,
+    highlight = {
+      before = "",
+      keyword = "wide",
+      after = "fg",
+      pattern = [[.*<(KEYWORDS)\s*:]],
+      comments_only = true,
+      max_line_len = 400,
+      exclude = {},
+    },
+    colors = {
+      error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+      warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+      info = { "#0db9d7" }, -- or "DiagnosticInfo"
+      hint = { "DiagnosticHint", "#10B981" },
+      default = { "Identifier", "#7C3AED" },
+    },
+    search = {
+      command = "rg",
+      args = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column" },
+      pattern = [[\b(KEYWORDS):]],
+    },
+  },
+}
+
+-- TODO: test this line whether the highlighting is done correctly. <10-07-22, pysan3>
+-- PERF: fully optimized
+-- NOTE: adding a note
+-- HACK: this should be implemented more carefully
+-- FIX: this needs fixing
+-- HINT: hoge
+-- WARNING: ???
---
+++ lua-plug/plugins/10-vim-matchup.lua
@@ -0,0 +1,10 @@
+return {
+  "andymass/vim-matchup",
+  event = "BufReadPost",
+  config = function()
+    vim.g.matchup_surround_enabled = 1
+    vim.g.matchup_delim_noskips = 1 -- recognize symbols within comments
+    vim.g.matchup_matchparen_enabled = 1
+    vim.g.matchup_matchparen_offscreen = {}
+  end,
+}
