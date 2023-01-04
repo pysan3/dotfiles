be2ee898361dd662cc48556f7d9516e75bbba041
pysan3 <pysan3@gmail.com>
2023-01-04 21:07:07 +0900

    install nvim-various-textobjs

---
+++ lua-plug/plugins/10-nvim-various-textobjs.lua
@@ -0,0 +1,9 @@
+return {
+  "chrisgrieser/nvim-various-textobjs",
+  event = "BufReadPre",
+  config = {
+    useDefaultKeymaps = true,
+  },
+}
+
+--- keybinds: https://github.com/chrisgrieser/nvim-various-textobjs#list-of-text-objects
