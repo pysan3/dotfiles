230061cabe7579b2d98b8e30c95891cd0c7f8db4
pysan3 <pysan3@gmail.com>
2023-01-04 21:07:12 +0900

    install chafa to render svg files in buffer

---
+++ lua-plug/plugins/50-chafa.lua
@@ -0,0 +1,9 @@
+return {
+  "princejoogie/chafa.nvim", -- render svg images in buffer
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "m00qek/baleia.nvim",
+  },
+  cmd = { "ViewImage" },
+  config = {},
+}
