23c4efc0e73a35bc3c96b61251d27d93a435e34e
pysan3 <pysan3@gmail.com>
2023-01-13 11:04:15 +0900

    add setup config for neoclip

--- lua-plug/plugins/20-nvim-neoclip.lua
+++ lua-plug/plugins/20-nvim-neoclip.lua
@@ -10,14 +10,56 @@ end
 return {
   "AckslD/nvim-neoclip.lua",
   dependencies = {
+    { 'nvim-telescope/telescope.nvim' },
     { "tami5/sqlite.lua" },
   },
   keys = {
-    { "<Leader>fb", extensions("neoclip", "neoclip") },
-    { "<Leader>fq", extensions("neoclip", "macroscope") },
+    { vim.g.personal_options.prefix.telescope .. "g", extensions("neoclip", "neoclip") },
+    { vim.g.personal_options.prefix.telescope .. "q", extensions("neoclip", "macroscope") },
     { "<C-y>", function()
       vim.cmd("stopinsert")
       extensions("neoclip", "neoclip")()
+      vim.cmd("startinsert")
     end, mode = "i" },
-  }
+  },
+  config = {
+    history = 1000,
+    preview = true,
+    content_spec_column = true,
+    db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+    enable_persistent_history = true,
+    enable_macro_history = true,
+    continuous_sync = true,
+    on_select = { move_to_front = true, set_reg = true },
+    on_paste = { move_to_front = true, set_reg = true },
+    on_replay = { move_to_front = true, set_reg = true },
+    keys = {
+      telescope = {
+        i = {
+          select = "<cr>",
+          paste = "<c-o>",
+          paste_behind = "<c-i>",
+          replay = "<c-q>", -- replay a macro
+          delete = "<c-d>", -- delete an entry
+          custom = {},
+        },
+        n = {
+          select = "<cr>",
+          paste = "p",
+          paste_behind = "P",
+          replay = "q",
+          delete = "d",
+          custom = {},
+        },
+      },
+    },
+    filter = function(data)
+      for _, entry in ipairs(data.event.regcontents) do
+        if vim.fn.match(entry, [[^\s*$]]) == -1 then
+          return true
+        end
+      end
+      return false
+    end,
+  },
 }
