80343141eaa17f1085c51186e516795bfafaa0f6
pysan3 <pysan3@gmail.com>
2023-01-29 22:10:49 +0900

    enable vim-matchup

--- lua-plug/plugins/10-vim-matchup.lua
+++ lua-plug/plugins/10-vim-matchup.lua
@@ -2,7 +2,6 @@ return {
   "andymass/vim-matchup",
   event = { "VeryLazy" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  enabled = false,
   init = function()
     vim.g.matchup_surround_enabled = 1
     vim.g.matchup_delim_noskips = 1 -- recognize symbols within comments
