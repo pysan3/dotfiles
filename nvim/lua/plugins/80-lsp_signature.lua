826893e4c7cfdda0514d4d4e4aaccd81f94c8d7f
pysan3 <pysan3@gmail.com>
2023-02-10 14:50:23 +0900

    update config for lsp_signature

--- nvim/lua/plugins/80-lsp_signature.lua
+++ nvim/lua/plugins/80-lsp_signature.lua
@@ -1,10 +1,18 @@
 return {
   "ray-x/lsp_signature.nvim", -- show hints when writing function arguments
-  event = { "InsertEnter", "FocusLost", "CursorHold" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "folke/noice.nvim",
+  },
+  event = { "InsertEnter", "VeryLazy" },
   config = {
+    bind = true,
+    noice = true,
     max_height = 5,
     zindex = 1,
-    floating_window_off_x = vim.fn.max(vim.opt.colorcolumn:get()) - vim.fn.getcurpos()[3], -- put lsp hints on colorcolumn
+    floating_window_off_x = function() -- put lsp hints on colorcolumn
+      return vim.fn.max(vim.opt.colorcolumn:get()) - vim.fn.getcurpos()[3]
+    end,
     extra_trigger_chars = { "(", ",", "\n" },
   },
 }
