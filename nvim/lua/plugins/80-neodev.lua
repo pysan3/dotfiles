6f90d27167e6a88f911c667174b0a6a7f9969cc8
pysan3 <pysan3@gmail.com>
2023-02-13 16:20:43 +0900

    enable neodev in local.lua

--- nvim/lua/plugins/80-neodev.lua
+++ nvim/lua/plugins/80-neodev.lua
@@ -4,6 +4,8 @@ return {
     override = function(root_dir, options)
       if root_dir:find("dotfiles") then
         options.enabled = true
+      elseif vim.g.personal_module.exists(root_dir .. "/.vim/local.lua") then
+        options.enabled = true
       end
     end,
   },
