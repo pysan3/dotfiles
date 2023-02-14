463639763d7f71e5cb8651953a119c57ca170da0
pysan3 <pysan3@gmail.com>
2023-02-14 17:58:08 +0900

    specify hlargs color based on colorscheme

--- nvim/lua/plugins/10-hlargs.lua
+++ nvim/lua/plugins/10-hlargs.lua
@@ -1,7 +1,12 @@
+local color_loopup = {
+  kanagawa = "#E6C384",
+}
+
 return {
   "m-demare/hlargs.nvim",
   event = "VeryLazy",
   opts = {
+    color = color_loopup[vim.g.personal_options.colorscheme],
     excluded_argnames = {
       usages = {
         lua = { "self", "use" },
