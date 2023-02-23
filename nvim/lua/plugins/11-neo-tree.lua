28c11083f37aee1cee63fbdff47d2b85fbc92836
pysan3 <pysan3@gmail.com>
2023-02-23 20:39:08 +0900

    modify window width on neo-tree open

--- nvim/lua/plugins/11-neo-tree.lua
+++ nvim/lua/plugins/11-neo-tree.lua
@@ -247,6 +247,22 @@ return {
           require("neo-tree").close_all()
         end,
       },
+      {
+        event = "neo_tree_window_after_open",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd.wincmd("=")
+          end
+        end,
+      },
+      {
+        event = "neo_tree_window_after_close",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd.wincmd("=")
+          end
+        end,
+      },
     },
   },
 }
