2d80de334bccf8ed6740dd7184107f47d753e218
pysan3 <pysan3@gmail.com>
2023-03-26 22:29:14 +0900

    update neorg config for nicer usage

--- nvim/lua/plugins/60-neorg.lua
+++ nvim/lua/plugins/60-neorg.lua
@@ -1,3 +1,11 @@
+local M = {
+  "nvim-neorg/neorg",
+  ft = "norg",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-cmp", "mason.nvim", "nvim-lua/plenary.nvim" },
+  build = ":Neorg sync-parsers",
+  cmd = "Neorg",
+}
+
 local function list_workspaces(w_dirs)
   local res = {}
   for _, w in ipairs(w_dirs) do
@@ -6,42 +14,29 @@ local function list_workspaces(w_dirs)
   return { workspaces = res }
 end
 
-return {
-  "nvim-neorg/neorg",
-  ft = "norg",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-cmp", "mason.nvim", "nvim-lua/plenary.nvim" },
-  build = ":Neorg sync-parsers",
-  cmd = "Neorg",
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {},
-        ["core.keybinds"] = {
-          -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
-          config = {
-            default_keybinds = true,
-            neorg_leader = "<Leader><Leader>",
-          },
-        },
-        ["core.norg.qol.toc"] = {},
-        ["core.norg.journal"] = {},
-        ["core.norg.dirman"] = {
-          config = list_workspaces({
-            "wiki",
-            "work",
-          }),
-        },
-        ["core.norg.concealer"] = {},
-        ["core.norg.completion"] = {
-          config = { engine = "nvim-cmp" },
-        },
-        ["core.integrations.nvim-cmp"] = {},
-        ["core.presenter"] = {
-          config = {
-            zen_mode = "zen-mode",
-          },
-        },
-      },
-    })
-  end,
+M.opts = { load = {} }
+M.opts.load["core.defaults"] = {}
+M.opts.load["core.norg.concealer"] = { config = { icon_preset = "diamond" } }
+M.opts.load["core.norg.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } }
+M.opts.load["core.norg.esupports.metagen"] = {}
+M.opts.load["core.integrations.nvim-cmp"] = {}
+M.opts.load["core.norg.qol.toc"] = {}
+M.opts.load["core.norg.qol.todo_items"] = {}
+M.opts.load["core.export"] = {}
+M.opts.load["core.presenter"] = { config = { zen_mode = "zen-mode" } }
+M.opts.load["core.norg.journal"] = {}
+M.opts.load["core.norg.dirman"] = {
+  config = list_workspaces({
+    "wiki",
+    "work",
+  }),
 }
+M.opts.load["core.keybinds"] = {
+  -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+  config = {
+    default_keybinds = true,
+    neorg_leader = "<Leader><Leader>",
+  },
+}
+
+return M
