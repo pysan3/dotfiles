c9cd05293e3f04665936d68ee623acedb84ac3bf
pysan3 <pysan3@gmail.com>
2023-03-26 22:03:10 +0900

    update config for cmp-dictionary due to deprecated options

--- nvim/lua/plugins/70-nvim-cmp-plugins.lua
+++ nvim/lua/plugins/70-nvim-cmp-plugins.lua
@@ -2,33 +2,15 @@ local M = {}
 
 table.insert(M, { "uga-rosa/cmp-dictionary" })
 M[#M].config = function()
-  -- from cmp-dictionary
-  local dict_source = {}
-  -- add my spell lists; $XDG_CONFIG_HOME/nvim/spell/*.add
-  for filepath in string.gmatch(vim.fn.glob(vim.env.XDG_CONFIG_HOME .. "/nvim/spell/*.add"), "[^\n]+") do
-    table.insert(dict_source, filepath)
-  end
-  -- add system installed dictionaries
-  local share_dict_source = {
-    "words",
-    "american-english", -- wamerican
-    -- "american-english-insane", -- wamerican-insane
-    -- "ngerman", -- wngerman
-  }
-  for _, source in ipairs(share_dict_source) do
-    if vim.fn.filereadable(vim.fn.expand("/usr/share/dict/" .. source)) ~= 0 then
-      table.insert(dict_source, "/usr/share/dict/" .. source)
-    end
-  end
-
+  vim.opt_global.dictionary = "/usr/share/dict/words"
   require("cmp_dictionary").setup({
-    dic = {
-      ["*"] = dict_source,
-    },
-    first_case_insensitive = true,
-    async = true,
+    exact = 2,
+    first_case_insensitive = false,
+    document = false,
+    async = false,
+    max_items = -1,
     capacity = 5,
-    max_items = 1000,
+    debug = false,
   })
 end
 
