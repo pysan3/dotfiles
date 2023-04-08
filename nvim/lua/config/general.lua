c4cb3bc8bdd3c40ceaf4510042b3f20c930d34c1
pysan3 <pysan3@gmail.com>
2023-04-09 01:18:05 +0900

    no need to disable builting plugins

--- nvim/lua/config/general.lua
+++ nvim/lua/config/general.lua
@@ -142,26 +142,6 @@ vim.cmd([[
 highlight WinSeparator guibg=None
 ]])
 
--- disable builtin plugins
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_logipat = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_spellfile_plugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-
 vim.api.nvim_create_autocmd("VimResized", {
   desc = "Resize Splits Automatically for Tmux",
   group = vim.api.nvim_create_augroup("WinAlignInTmux", { clear = true }),
