89e3049e5e37741cc9b0d94d6707a66ad2784859
pysan3 <pysan3@gmail.com>
2023-04-08 23:52:45 +0900

    update barbecue config for more stability

--- nvim/lua/plugins/80-barbecue.lua
+++ nvim/lua/plugins/80-barbecue.lua
@@ -1,33 +1,17 @@
-local events = {
-  "BufWinEnter",
-  "CursorHold",
-  "InsertLeave",
-}
-if vim.fn.has('nvim-0.9') == 1 then
-  events[#events + 1] = "WinResized"
-end
-
 return {
   "utilyre/barbecue.nvim",
-  name = "barbecue",
-  version = "*",
   dependencies = {
     "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
+    "nvim-tree/nvim-web-devicons",
   },
-  event = events,
-  init = function()
-    vim.api.nvim_create_autocmd(events, {
-      group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-      callback = function()
-        require("barbecue")
-        require("barbecue.ui").update()
-      end,
-    })
-  end,
+  event = {
+    "BufWinEnter",
+    "CursorHold",
+    "InsertLeave",
+  },
   opts = {
-    create_autocmd = false, -- prevent barbecue from updating itself automatically
-    attach_navic = false,   -- prevent barbecue from automatically attaching nvim-navic
+    create_autocmd = true,
+    attach_navic = true,
     context_follow_icon_color = false,
     symbols = {
       modified = "● ",
