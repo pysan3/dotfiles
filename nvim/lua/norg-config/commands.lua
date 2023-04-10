2539e783392021edd6236ae134f3fdc8c15484da
pysan3 <pysan3@gmail.com>
2023-04-10 21:50:29 +0900

    add commands `Tomorrow` and `Yesterday` to open neorg journal

--- nvim/lua/norg-config/commands.lua
+++ nvim/lua/norg-config/commands.lua
@@ -2,12 +2,15 @@ M = {}
 
 M.subcmd_alias = function(_)
   vim.api.nvim_create_user_command("Metadata", "Neorg inject-metadata", { desc = "Neorg: alias to inject-metadata" })
-  vim.api.nvim_create_user_command("Today", function()
-    vim.cmd([[
-    Neorg journal today
-    Metadata
-    ]])
-  end, { desc = "Neorg: open today's journal", force = true })
+  local days = { "Yesterday", "Today", "Tomorrow" }
+  for _, cmd in ipairs(days) do
+    vim.api.nvim_create_user_command(cmd, function()
+      vim.cmd(([[
+      Neorg journal %s
+      Metadata
+      ]]):format(cmd:lower()))
+    end, { desc = "Neorg: open " .. cmd .. "'s journal", force = true })
+  end
 end
 
 M.setup = function(opts)
