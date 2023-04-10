74fa9299de50b87df5ba9ab99b35bd141e36cd4f
pysan3 <pysan3@gmail.com>
2023-04-10 22:47:41 +0900

    add commands and autocmds for norg files

--- nvim/lua/norg-config/commands.lua
+++ nvim/lua/norg-config/commands.lua
@@ -1,6 +1,6 @@
 M = {}
 
-M.subcmd_alias = function(_)
+local function subcmd_alias(_)
   vim.api.nvim_create_user_command("Metadata", "Neorg inject-metadata", { desc = "Neorg: alias to inject-metadata" })
   local days = { "Yesterday", "Today", "Tomorrow" }
   for _, cmd in ipairs(days) do
@@ -14,7 +14,7 @@ M.subcmd_alias = function(_)
 end
 
 M.setup = function(opts)
-  M.subcmd_alias(opts)
+  subcmd_alias(opts)
 end
 
 return M
