9bb17868331fb2c4a8161cedaf9fab82ea6c7743
pysan3 <pysan3@gmail.com>
2023-04-11 10:38:36 +0900

    save norg file automatically

--- nvim/lua/norg-config/commands.lua
+++ nvim/lua/norg-config/commands.lua
@@ -1,13 +1,17 @@
 M = {}
 
 local function subcmd_alias(_)
-  vim.api.nvim_create_user_command("Metadata", "Neorg inject-metadata", { desc = "Neorg: alias to inject-metadata" })
+  vim.api.nvim_create_user_command(
+    "Metadata",
+    "Neorg inject-metadata",
+    { desc = "Neorg: alias to inject-metadata", bar = true }
+  )
   local days = { "Yesterday", "Today", "Tomorrow" }
   for _, cmd in ipairs(days) do
     vim.api.nvim_create_user_command(cmd, function()
       pcall(vim.cmd, [[Neorg journal ]] .. cmd:lower()) ---@diagnostic disable-line
       vim.schedule(function()
-        vim.cmd([[Metadata]])
+        vim.cmd([[Metadata | w]])
         vim.cmd([[normal ]h]h]]) -- move down to {** Daily Review}
       end)
     end, { desc = "Neorg: open " .. cmd .. "'s journal", force = true })
