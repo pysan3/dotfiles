28414792beed0a2ea1ab5e786b5d191ac0bd07c0
pysan3 <pysan3@gmail.com>
2023-04-13 15:16:35 +0900

    update sudo_setup.zsh with more dependencies

--- sudo_setup.zsh
+++ sudo_setup.zsh
@@ -11,19 +11,21 @@ unset DOTFILES_FUNCTIONS && source "$DOTFILES/functions.zsh"
 setopt sh_word_split
 
 if command -v 'apt-get' &>/dev/null || checkyes 'apt-get available?'; then
-  # python build dependencies
+  # install_base.zsh dependencies
+  sudo apt-get install -y moreutils
+  # python build dependencies (for pyenv)
   sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git python3-distutils-extra python-distutils-extra
-  # btop dependencies
+  # nvim dependencies (build latest on my own)
+  sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+  # tmux dependencies (build latest on my own)
+  sudo apt-get install -y libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config
+  # words (use for spell check)
+  sudo apt-get install -y wamerican
+  # btop dependencies (better monitoring tool)
   sudo apt-get install -y coreutils sed git build-essential
   sudo apt-get install -y gcc-11 g++-11 || sudo apt-get install -y gcc-10 g++-10
-  # bmon dependencies
+  # bmon dependencies (better monitoring tool)
   sudo apt-get install -y build-essential make libconfuse-dev libnl-3-dev libnl-route-3-dev libncurses-dev pkg-config dh-autoreconf
-  # nvim dependencies
-  sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-  # tmux dependencies
-  sudo apt-get install -y libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config
-  # words
-  sudo apt-get install -y wamerican
 fi
 
 if command -v 'pacman' &>/dev/null || checkyes 'pacman available?'; then
