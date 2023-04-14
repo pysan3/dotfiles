8b85ce6e821e00e84bd026feb1f9628c749fa2df
pysan3 <pysan3@gmail.com>
2023-04-14 17:11:49 +0900

    update norg snippets and commands

--- nvim/lua/norg-config/commands.lua
+++ nvim/lua/norg-config/commands.lua
@@ -1,11 +1,7 @@
 M = {}
 
 local function subcmd_alias(_)
-  vim.api.nvim_create_user_command(
-    "Metadata",
-    "Neorg inject-metadata",
-    { desc = "Neorg: alias to inject-metadata", bar = true }
-  )
+  vim.api.nvim_create_user_command("Metadata", "Neorg inject-metadata", { desc = "Neorg: inject-metadata", bar = true })
   local days = { "Yesterday", "Today", "Tomorrow" }
   for _, cmd in ipairs(days) do
     vim.api.nvim_create_user_command(cmd, function()
