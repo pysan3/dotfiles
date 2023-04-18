560b159bc893d8b1d167a9342fdf2588125fb21f
pysan3 <pysan3@gmail.com>
2023-04-19 03:00:25 +0900

    refer to file title when creating norg journal from templates

---
+++ nvim/lua/norg-config/templates.lua
@@ -0,0 +1,46 @@
+---@diagnostic disable
+-- stylua: ignore start
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+local m = require("luasnip.extras").m
+local lambda = require("luasnip.extras").l
+local postfix = require("luasnip.extras.postfix").postfix
+
+local e = function(trig, name, dscr, wordTrig, regTrig, docstring, docTrig, hidden, priority)
+  local ret = { trig = trig, name = name, dscr = dscr }
+  if wordTrig ~= nil then ret["wordTrig"] = wordTrig end
+  if regTrig ~= nil then ret["regTrig"] = regTrig end
+  if docstring ~= nil then ret["docstring"] = docstring end
+  if docTrig ~= nil then ret["docTrig"] = docTrig end
+  if hidden ~= nil then ret["hidden"] = hidden end
+  if priority ~= nil then ret["priority"] = priority end
+  return ret
+end
+-- stylua: ignore end
+---@diagnostic enable
+
+local tmpls = require("neorg.modules.external.templates.default_snippets")
+local M = {}
+
+M.keywords = {
+  TITLE_YESTERDAY = function()
+    return i(1, tmpls.parse_date(-1, tmpls.file_title()))
+  end,
+  TITLE_TOMORROW = function()
+    return i(1, tmpls.parse_date(1, tmpls.file_title()))
+  end,
+}
+
+return M
--- nvim/lua/plugins/60-neorg.lua
+++ nvim/lua/plugins/60-neorg.lua
@@ -32,48 +32,53 @@ local function list_workspaces(w_dirs)
   return { workspaces = res }
 end
 
-local plugins = {
-  ["core.defaults"] = {},
-  ["core.norg.concealer"] = { config = { icon_preset = "diamond" } },
-  ["core.norg.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
-  ["core.norg.esupports.metagen"] = { config = { type = "empty", update_date = true } },
-  ["core.integrations.nvim-cmp"] = {},
-  ["core.norg.qol.toc"] = {},
-  ["core.norg.qol.todo_items"] = {},
-  ["core.looking-glass"] = {},
-  ["core.export"] = {},
-  ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
-  ["core.norg.journal"] = {
-    config = {
-      strategy = "flat",
-      template_name = "../templates/journal.norg",
-      workspace = M.default_workspace,
+local function load_plugins()
+  return {
+    ["core.defaults"] = {},
+    ["core.norg.concealer"] = { config = { icon_preset = "diamond" } },
+    ["core.norg.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+    ["core.norg.esupports.metagen"] = { config = { type = "empty", update_date = true } },
+    ["core.integrations.nvim-cmp"] = {},
+    ["core.norg.qol.toc"] = {},
+    ["core.norg.qol.todo_items"] = {},
+    ["core.looking-glass"] = {},
+    ["core.export"] = {},
+    ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+    ["core.norg.journal"] = {
+      config = {
+        strategy = "flat",
+        template_name = "../templates/journal.norg",
+        workspace = M.default_workspace,
+      },
     },
-  },
-  ["core.norg.dirman"] = {
-    config = list_workspaces({
-      M.default_workspace,
-      "Works",
-    }),
-  },
-  ["core.keybinds"] = {
-    -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
-    config = {
-      default_keybinds = true,
-      neorg_leader = "<Leader><Leader>",
-      hook = require("norg-config.keybinds").hook,
+    ["core.norg.dirman"] = {
+      config = list_workspaces({
+        M.default_workspace,
+        "Works",
+      }),
     },
-  },
-  ["external.zettelkasten"] = {},
-  ["external.templates"] = {
-    config = {
-      templates_dir = vim.fn.stdpath("config") .. "/templates/norg",
+    ["core.keybinds"] = {
+      -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+      config = {
+        default_keybinds = true,
+        neorg_leader = "<Leader><Leader>",
+        hook = require("norg-config.keybinds").hook,
+      },
     },
-  },
-}
+    ["external.zettelkasten"] = {},
+    ["external.templates"] = {
+      config = {
+        templates_dir = vim.fn.stdpath("config") .. "/templates/norg",
+        keywords = require("norg-config.templates").keywords,
+      },
+    },
+  }
+end
 
-M.opts = {
-  load = plugins,
-}
+M.config = function()
+  require("neorg").setup({
+    load = load_plugins(),
+  })
+end
 
 return M
