7374973b090ee0bcbf0dcc0e359370282e7ce41a
pysan3 <pysan3@gmail.com>
2023-04-23 01:00:40 +0900

    update lsp_signature config to use `x_off` to calc window pos

--- nvim/lua/plugins/80-lsp_signature.lua
+++ nvim/lua/plugins/80-lsp_signature.lua
@@ -14,12 +14,11 @@ return {
     floating_window_above_cur_line = true,
     floating_window_off_x = function(info)
       local cur_pos = vim.api.nvim_win_get_cursor(0)[2] + 1
-      local woff = info.woff or -vim.trim(vim.api.nvim_get_current_line():sub(1, cur_pos)):len()
+      local x_off = info.x_off or -vim.trim(vim.api.nvim_get_current_line():sub(1, cur_pos)):len()
       local win_width = vim.api.nvim_win_get_width(0) - vim.g.personal_options.signcolumn_length
       local origin = math.min(120, win_width)
-      return origin - woff - cur_pos
+      return origin - x_off - cur_pos
     end,
     extra_trigger_chars = { "(", ",", "\n" },
-    toggle_key = "<C-k>",
   },
 }
