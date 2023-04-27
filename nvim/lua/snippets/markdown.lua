625cae134e3a96d0b1c1416233a802df8f572f57
pysan3 <pysan3@gmail.com>
2023-04-27 17:57:59 +0900

    add markdown snippets to insert html tags

---
+++ nvim/lua/snippets/markdown.lua
@@ -0,0 +1,65 @@
+---@diagnostic disable
+-- stylua: ignore start
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+local m = require("luasnip.extras").m
+local lambda = require("luasnip.extras").l
+local postfix = require("luasnip.extras.postfix").postfix
+
+local snippets, autosnippets = {}, {}
+local e = function(trig, name, dscr, wordTrig, regTrig, docstring, docTrig, hidden, priority)
+  local ret = { trig = trig, name = name, dscr = dscr }
+  if wordTrig ~= nil then ret["wordTrig"] = wordTrig end
+  if regTrig ~= nil then ret["regTrig"] = regTrig end
+  if docstring ~= nil then ret["docstring"] = docstring end
+  if docTrig ~= nil then ret["docTrig"] = docTrig end
+  if hidden ~= nil then ret["hidden"] = hidden end
+  if priority ~= nil then ret["priority"] = priority end
+  return ret
+end
+-- stylua: ignore end
+---@diagnostic enable
+
+snippets[#snippets + 1] = s(
+  e("divbig", "div with large gap"),
+  fmt(
+    [[
+<div>
+
+{}
+
+</div>
+  ]],
+    i(0)
+  )
+)
+
+snippets[#snippets + 1] = s(
+  e("divsmall", "div with no gap but with class"),
+  fmt(
+    [[
+<div{class}>
+
+{cursor}
+
+</div>
+  ]],
+    {
+      cursor = i(0),
+      class = c(1, { t(""), { t([[ class="]]), i(1, "columns"), t([["]]) } }),
+    }
+  )
+)
+return snippets, autosnippets
