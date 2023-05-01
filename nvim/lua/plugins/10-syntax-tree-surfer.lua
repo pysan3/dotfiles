869aa2bfb40f4419099ca356310507fb145ff82f
pysan3 <pysan3@gmail.com>
2023-05-01 19:24:13 +0900

    add description of syntax-tree-surfer keymaps

--- nvim/lua/plugins/10-syntax-tree-surfer.lua
+++ nvim/lua/plugins/10-syntax-tree-surfer.lua
@@ -1,27 +1,28 @@
-local function keymaps(lhs, rhs, modes, is_expr, noremap)
-  return { lhs, rhs, mode = modes, expr = is_expr, noremap = noremap, silent = true }
+local function keymaps(lhs, rhs, modes, is_expr, noremap, desc)
+  return { lhs, rhs, mode = modes, expr = is_expr, noremap = noremap, silent = true, desc = desc }
 end
 
 return {
   "ziontee113/syntax-tree-surfer",
   keys = {
+    -- Swap The Master Node relative to the cursor with it's siblings, Dot Repeatable
     keymaps("vU", function()
       vim.opt.opfunc = "v:lua.STSSwapUpNormal_Dot"
       return "g@l"
-    end, "n", true, false),
+    end, "n", true, false, "v:lua.STSSwapUpNormal_Dot"),
     keymaps("vD", function()
       vim.opt.opfunc = "v:lua.STSSwapDownNormal_Dot"
       return "g@l"
-    end, "n", true, false),
+    end, "n", true, false, "v:lua.STSSwapDownNormal_Dot"),
     -- Swap Current Node at the Cursor with it's siblings, Dot Repeatable
     keymaps("vd", function()
       vim.opt.opfunc = "v:lua.STSSwapCurrentNodeNextNormal_Dot"
       return "g@l"
-    end, "n", true, false),
+    end, "n", true, false, "v:lua.STSSwapCurrentNodeNextNormal_Dot"),
     keymaps("vd", function()
       vim.opt.opfunc = "v:lua.STSSwapCurrentNodePrevNormal_Dot"
       return "g@l"
-    end, "n", true, false),
+    end, "n", true, false, "v:lua.STSSwapCurrentNodePrevNormal_Dot"),
     -- Visual Selection from Normal Mode
     keymaps("vx", "<Cmd>STSSelectMasterNode<CR>", "n", false, true),
     keymaps("vn", "<Cmd>STSSelectCurrentNode<CR>", "n", false, true),
