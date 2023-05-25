ae8f9df50a65a455b9f9e7de0490da274efe3c8f
pysan3 <pysan3@gmail.com>
2023-05-25 20:39:50 +0900

    fast load autosession

--- nvim/lua/plugins/20-autosession.lua
+++ nvim/lua/plugins/20-autosession.lua
@@ -1,8 +1,11 @@
+local session_exists = vim.g.personal_module.exists(".vim/session.vim")
+
 return {
   "pysan3/autosession.nvim", -- restore previous session
   event = { "VeryLazy" },
+  lazy = not session_exists,
   -- dev = true,
-  dependencies = { "mhinz/vim-startify" },
+  dependencies = { "mhinz/vim-startify", cond = not session_exists },
   init = function()
     vim.api.nvim_create_user_command("CL", "AutoSessionSave <bar> :wqa", {})
     vim.api.nvim_create_user_command("A", ":qa", {})
