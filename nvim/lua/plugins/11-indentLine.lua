b30d2ef36cc8f666e78f3214e3c493d8464e2086
pysan3 <pysan3@gmail.com>
2023-06-05 00:54:34 +0900

    fix bug

--- nvim/lua/plugins/11-indentLine.lua
+++ nvim/lua/plugins/11-indentLine.lua
@@ -4,7 +4,7 @@ return {
   init = function()
     vim.g.indentLine_char = "▏"
     vim.g.indentLine_concealcursor = ""
-    vim.g.indentLine_bufTypeExclude = { "help", "terminal", "neo-tree" }
-    vim.g.indentLine_fileTypeExclude = { "noice" }
+    vim.g.indentLine_bufTypeExclude = { "help", "terminal" }
+    vim.g.indentLine_fileTypeExclude = { "noice", "neo-tree" }
   end,
 }
