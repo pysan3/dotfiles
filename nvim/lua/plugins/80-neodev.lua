30502e97e7c4b52c8ef1e003c0569edc2a4ddab2
pysan3 <pysan3@gmail.com>
2023-06-05 21:45:12 +0900

    check for .nvim.lua for 'exrc' instead of local config

--- nvim/lua/plugins/80-neodev.lua
+++ nvim/lua/plugins/80-neodev.lua
@@ -4,7 +4,7 @@ return {
     override = function(root_dir, options)
       if root_dir:find("dotfiles") then
         options.enabled = true
-      elseif vim.g.personal_module.exists(root_dir .. "/.vim/local.lua") then
+      elseif vim.g.personal_module.exists(root_dir .. "/.nvim.lua") then
         options.enabled = true
       end
     end,
