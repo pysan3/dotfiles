53b0f7786a716343ebea86d870afc8cadb57d552
pysan3 <pysan3@gmail.com>
2023-06-20 04:38:05 +0900

    deprecate vim-expand-region

--- nvim/lua/plugins/50-vim-expand-region.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "terryma/vim-expand-region",
-  keys = {
-    { "v", "<Plug>(expand_region_expand)", mode = "x", remap = true, desc = "<Plug>(expand_region_expand)" },
-    { "<C-v>", "<Plug>(expand_region_shrink)", mode = "x", remap = true, desc = "<Plug>(expand_region_shrink)" },
-  },
-}
