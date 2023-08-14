937f6beec1c65e866ad40d434737c7743792cfed
pysan3 <pysan3@gmail.com>
2023-08-14 11:25:07 +0900

    update config for inhay hints

--- nvim/lua/plugins/80-inlay-hints.lua
+++ nvim/lua/plugins/80-inlay-hints.lua
@@ -1,6 +1,23 @@
 return {
   "simrat39/inlay-hints.nvim",
   opts = {
-    only_current_line = false,
+    only_current_line = true,
+    renderer = "inlay-hints.render.eol",
+    eol = {
+      right_align = false,
+      right_align_padding = 7,
+      parameter = {
+        separator = ", ",
+        format = function(hints)
+          return string.format(" <- (%s)", hints)
+        end,
+      },
+      type = {
+        separator = ", ",
+        format = function(hints)
+          return string.format(" » (%s)", hints)
+        end,
+      },
+    },
   },
 }
