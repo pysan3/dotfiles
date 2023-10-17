b3a471bf9ab57cdb9f568c5886b0c7ceec9ad2aa
pysan3 <pysan3@gmail.com>
2023-10-17 16:20:05 +0900

    delay highlights by matchup for performance

--- nvim/lua/plugins/10-vim-matchup.lua
+++ nvim/lua/plugins/10-vim-matchup.lua
@@ -7,9 +7,12 @@ return {
     vim.g.matchup_delim_noskips = 1 -- recognize symbols within comments
     vim.g.matchup_matchparen_enabled = 1
     vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    vim.g.matchup_matchparen_deferred = 1
+    vim.g.matchup_matchparen_timeout = 300
+    vim.g.matchup_matchparen_insert_timeout = 60
   end,
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter.configs").setup({ ---@diagnostic disable-line
       matchup = {
         enable = true,
         disable_virtual_text = false,
