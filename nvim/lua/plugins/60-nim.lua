3a75eee335b6226564a8b65cc5a168a8819414cb
pysan3 <pysan3@gmail.com>
2023-11-04 20:15:24 +0900

    add nim specific keybinds to restart lang server

--- nvim/lua/plugins/60-nim.lua
+++ nvim/lua/plugins/60-nim.lua
@@ -1,4 +1,16 @@
 return {
   "alaviss/nim.nvim",
   ft = { "nim" },
+  config = function()
+    local aug = vim.api.nvim_create_augroup("NimRestartKeybinds", { clear = true })
+    vim.api.nvim_create_autocmd("Filetype", {
+      pattern = "nim",
+      group = aug,
+      desc = "Assign keybind to reset lang server attached to this buffer.",
+      callback = function()
+        vim.keymap.set("n", "<Leader>r", "<Cmd>LspStart<CR>", { buffer = 0, silent = true })
+        vim.keymap.set("n", "<Leader>R", "<Cmd>LspRestart<CR>", { buffer = 0, silent = true })
+      end,
+    })
+  end,
 }
