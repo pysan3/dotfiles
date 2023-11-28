06901b200fe33fe51aee810008f639401f46f2dd
pysan3 <pysan3@gmail.com>
2023-11-28 21:17:00 +0900

    make stylua happy

--- nvim/lua/plugins/70-LuaSnip.lua
+++ nvim/lua/plugins/70-LuaSnip.lua
@@ -219,6 +219,9 @@ M.config = function()
   end
 
   local function update_choice_popup(choiceNode)
+    if not current_win then
+      return
+    end
     vim.api.nvim_win_close(current_win.win_id, true)
     vim.api.nvim_buf_del_extmark(current_win.buf, current_nsid, current_win.extmark)
     local create_win = window_for_choiceNode(choiceNode)
@@ -228,6 +231,9 @@ M.config = function()
   end
 
   local function choice_popup_close()
+    if not current_win then
+      return
+    end
     vim.api.nvim_win_close(current_win.win_id, true)
     vim.api.nvim_buf_del_extmark(current_win.buf, current_nsid, current_win.extmark)
     -- now we are checking if we still have previous choice we were in after exit nested choice
