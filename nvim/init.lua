e2235fda27db51bd2816d28fcd9982835b5f7642
pysan3 <pysan3@gmail.com>
2023-12-04 21:58:31 +0900

    workaround deprecated VertSplit

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,7 @@ source $HOME/.config/nvim/keys/mappings.vim
 require("config.general")
 require("config.lazy-plug")
 vim.cmd.colorscheme(vim.g.personal_options.colorscheme)
+vim.cmd("hi default link WinSeparator VertSplit")
 
 pcall(require, "local") -- load "$HOME/.config/nvim/local.lua" if exists
 require("config.after")
