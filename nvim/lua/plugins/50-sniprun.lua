4a2bbed657f3d3aafa5a66a2dc38cb1b895fc006
pysan3 <pysan3@gmail.com>
2023-12-10 23:33:58 +0900

    disable sniprun for now

--- nvim/lua/plugins/50-sniprun.lua
+++ nvim/lua/plugins/50-sniprun.lua
@@ -2,6 +2,7 @@ return {
   "michaelb/sniprun",
   build = "sh ./install.sh 1",
   version = false,
+  enabled = false,
   keys = {
     { "<Leader>rf", "<Plug>SnipRun" },
     { "<Leader>r", "<Plug>SnipRunOperator" },
