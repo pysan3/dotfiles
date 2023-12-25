94f3741956b2a778793823ee844683786f300f10
pysan3 <pysan3@gmail.com>
2023-12-25 21:24:12 +0900

    install atool

--- sudo_setup.zsh
+++ sudo_setup.zsh
@@ -11,21 +11,17 @@ unset DOTFILES_FUNCTIONS && source "$DOTFILES/functions.zsh"
 setopt sh_word_split
 
 if command -v 'apt-get' &>/dev/null || checkyes 'apt-get available?'; then
+  alias apti="sudo apt-get install -y"
   # install_base.zsh dependencies
-  sudo apt-get install -y moreutils
+  apti moreutils atool
   # python build dependencies (for pyenv)
-  sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git python3-distutils-extra python-distutils-extra
+  apti make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git python3-distutils-extra python-distutils-extra
   # nvim dependencies (build latest on my own)
-  sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+  apti ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
   # tmux dependencies (build latest on my own)
-  sudo apt-get install -y libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config
+  apti libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config
   # words (use for spell check)
-  sudo apt-get install -y wamerican
-  # btop dependencies (better monitoring tool)
-  sudo apt-get install -y coreutils sed git build-essential
-  sudo apt-get install -y gcc-11 g++-11 || sudo apt-get install -y gcc-10 g++-10
-  # bmon dependencies (better monitoring tool)
-  sudo apt-get install -y build-essential make libconfuse-dev libnl-3-dev libnl-route-3-dev libncurses-dev pkg-config dh-autoreconf
+  apti wamerican
 fi
 
 if command -v 'pacman' &>/dev/null || checkyes 'pacman available?'; then
@@ -34,30 +30,36 @@ if command -v 'pacman' &>/dev/null || checkyes 'pacman available?'; then
     use_yay=true
   fi
   set -xe
-  sudo pacman --noconfirm -Syu
+  alias pacman="sudo pacman --noconfirm"
+  pacman -Syu
   # basics
-  sudo pacman --noconfirm -S base-devel neofetch git tmux vim curl moreutils
+  pacman -S base-devel neofetch git tmux vim curl moreutils atool
   # nvim dependencies
-  sudo pacman --noconfirm -S base-devel cmake unzip ninja tree-sitter curl
+  pacman -S base-devel cmake unzip ninja tree-sitter curl
   # python dependencies
-  sudo pacman --noconfirm -S base-devel openssl zlib xz tk python python-pip python-virtualenv python-pipenv \
+  pacman -S base-devel openssl zlib xz tk python python-pip python-virtualenv python-pipenv \
     python-gobject python-wxpython python-yaml python-xlib python-utils python-pyopenssl \
     poppler-glib python-distutils-extra python-pip python-gobject gtk3 python-cairo libhandy
   # texlive
-  # sudo pacman --noconfirm -S texlive-full
+  # pacman -S texlive-full
   # yay
   if [ "$use_yay" = true ]; then
-    command -v 'yay' &>/dev/null || sudo pacman --noconfirm -S yay
+    command -v 'yay' &>/dev/null || pacman -S yay
   else
     error 'yay not found'
     exit 1
   fi
+  alias yay="yay --noconfirm"
+  # utils
+  yay -S bmon btop
   # words
-  yay --noconfirm -S words
+  yay -S words
   # xdg portals
-  yay --noconfirm -S xdg-desktop-portal-kde xdg-desktop-portal-gtk xdg-desktop-portal-gnome
+  yay -S xdg-desktop-portal-kde xdg-desktop-portal-gtk xdg-desktop-portal-gnome
+  # plemol install
+  yay -S fontforge
   # delete all cache
-  sudo pacman --noconfirm -Scc
-  yay --noconfirm -Scc
+  pacman -Scc
+  yay -Scc
   set +xe
 fi
