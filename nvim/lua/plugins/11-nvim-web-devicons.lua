ee4bf4ef32d41d2f0bc969bbf4a3be4ac27152e1
pysan3 <pysan3@gmail.com>
2023-12-25 20:19:22 +0900

    update devicons config

--- nvim/lua/plugins/11-nvim-web-devicons.lua
+++ nvim/lua/plugins/11-nvim-web-devicons.lua
@@ -1,49 +1,46 @@
-local M = {
+return {
   "nvim-web-devicons",
+  config = function()
+    local devicons = require("nvim-web-devicons")
+    devicons.setup({
+      override = {
+        norg = {
+          icon = "◉", -- Default icon for Header 1
+          color = "#4878BE",
+          name = "Norg",
+        },
+        zwc = {
+          icon = "",
+          color = "#41535B",
+          name = "zCompile",
+        },
+        proto = {
+          icon = "",
+          color = "#FD5C78",
+          name = "Protobuf",
+        },
+        snippets = {
+          icon = "",
+          color = "#DBC63E",
+          name = "Snippets",
+        },
+        ["poetry.lock"] = {
+          icon = "󰌾",
+          color = "#41535B",
+          name = "PoetryLockFile",
+        },
+      },
+      default = true,
+    })
+    local default_icons = devicons.get_icons()
+    devicons.set_icon({
+      pyi = default_icons.pyd,
+      latex = default_icons.tex,
+      [".latexmkrc"] = default_icons.tex,
+      sty = default_icons.tex,
+      [".pylintrc"] = default_icons.toml,
+      [".python-version"] = default_icons.toml,
+      ["Makefile"] = default_icons.makefile,
+    })
+  end,
 }
-
-M.config = function()
-  local devicons = require("nvim-web-devicons")
-  devicons.setup({
-    override = {
-      norg = {
-        icon = "◉", -- Default icon for Header 1
-        color = "#4878BE",
-        name = "Norg",
-      },
-      zwc = {
-        icon = "",
-        color = "#41535B",
-        name = "zCompile",
-      },
-      proto = {
-        icon = "",
-        color = "#FD5C78",
-        name = "Protobuf",
-      },
-      snippets = {
-        icon = "",
-        color = "#DBC63E",
-        name = "Snippets",
-      },
-      ["poetry.lock"] = {
-        icon = "󰌾",
-        color = "#41535B",
-        name = "PoetryLockFile",
-      },
-    },
-    default = true,
-  })
-  local default_icons = devicons.get_icons()
-  devicons.set_icon({
-    pyi = default_icons.pyd,
-    latex = default_icons.tex,
-    [".latexmkrc"] = default_icons.tex,
-    sty = default_icons.tex,
-    [".pylintrc"] = default_icons.toml,
-    [".python-version"] = default_icons.toml,
-    ["Makefile"] = default_icons.makefile,
-  })
-end
-
-return M
