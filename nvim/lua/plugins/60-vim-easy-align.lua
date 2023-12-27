547e01d5134f1fa5e5bf760607bcf4a188ab2a91
pysan3 <pysan3@gmail.com>
2023-12-27 12:08:26 +0900

    load EasyAlign in norg files as well

--- nvim/lua/plugins/60-vim-easy-align.lua
+++ nvim/lua/plugins/60-vim-easy-align.lua
@@ -1,6 +1,6 @@
 return {
   "junegunn/vim-easy-align",
-  ft = vim.g.personal_module.md(),
+  ft = vim.g.personal_module.md({ "norg" }),
   cmd = { "EasyAlign" },
   keys = {
     { "ga", "<Plug>(EasyAlign)", noremap = false, mode = { "n", "x" } },
