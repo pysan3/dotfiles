1cfca77a0061aa65c67c9c60886a76c37017346b
pysan3 <pysan3@gmail.com>
2024-01-22 02:02:22 +0900

    fix lua_ls libs path

--- nvim/lua/config/general.lua
+++ nvim/lua/config/general.lua
@@ -157,7 +157,7 @@ vim.g.personal_module = {
       })
     end
   end,
-  luarocks_base = vim.fn.fnamemodify("~/.luarocks/share/lua/5.1", ":p:h"),
+  luarocks_base = vim.fn.fnamemodify("~/.luarocks/share/lua/5.1", ":p"),
   load_luarocks = function()
     if not string.find(package.path, "luarocks") then
       local luarocks_base = vim.g.personal_module.luarocks_base
