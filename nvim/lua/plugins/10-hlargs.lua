b0c2fbf7e855b57b25e554d1a79be56844c076bd
pysan3 <pysan3@gmail.com>
2024-02-07 14:55:24 +0900

    increase hlargs priority to overwrite lsp semantics

--- nvim/lua/plugins/10-hlargs.lua
+++ nvim/lua/plugins/10-hlargs.lua
@@ -3,6 +3,10 @@ return {
   event = "VeryLazy",
   opts = {
     color = vim.g.personal_lookup.get("hlargs", vim.g.personal_options.colorscheme),
+    hl_priority = 10000,
+    extras = {
+      named_parameters = true,
+    },
     excluded_argnames = {
       usages = {
         lua = { "self", "use" },
