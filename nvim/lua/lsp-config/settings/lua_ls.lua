29b76e6cec2a35b0eece566f62051b0902e241d4
pysan3 <pysan3@gmail.com>
2024-02-16 01:16:57 +0900

    properly detect luassert

--- nvim/lua/lsp-config/settings/lua_ls.lua
+++ nvim/lua/lsp-config/settings/lua_ls.lua
@@ -2,6 +2,7 @@ local libs = vim.api.nvim_get_runtime_file("", true)
 libs[#libs + 1] = vim.g.personal_module.luarocks_base
 libs[#libs + 1] = vim.fn.fnamemodify("lua_modules/share/lua/5.1", ":p")
 libs[#libs + 1] = [[${3rd}/busted/library]]
+libs[#libs + 1] = [[${3rd}/luassert/library]]
 
 return {
   settings = {
