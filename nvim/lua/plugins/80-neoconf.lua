71a0d79fe3e5ebdd0172636a1e144d947fa5663b
pysan3 <pysan3@gmail.com>
2024-02-24 13:48:30 +0900

    always load neoconf anyways

--- nvim/lua/plugins/80-neoconf.lua
+++ nvim/lua/plugins/80-neoconf.lua
@@ -2,6 +2,12 @@ return {
   "folke/neoconf.nvim",
   cmd = { "Neoconf" },
   opts = {
+    override = function(root_dir, options)
+      if vim.loop.fs_stat(root_dir .. "/lua") then
+        options.plugins = true
+        options.enabled = true
+      end
+    end,
     plugins = {
       lua_ls = {
         enabled = true,
