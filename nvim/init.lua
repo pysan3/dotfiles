764b22c3f3436adba416aa303e8a7a7fa156c0a5
takuto <takutoitoi914@ruri.waseda.jp>
2024-02-26 13:18:46 +0000

    revert silly mistake

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,20 +3,12 @@ source $HOME/.config/nvim/general/settings.vim
 source $HOME/.config/nvim/keys/mappings.vim
 ]])
 
-vim.cmd([[
-source $HOME/dotfiles/static/source_norg_ts.lua
-]])
-
 require("config.general")
 require("config.lazy-plug")
 vim.cmd.colorscheme(vim.g.personal_options.colorscheme)
 vim.cmd("hi default link WinSeparator VertSplit")
 vim.cmd("hi! link WinBar StatusLineNC")
 vim.cmd("hi! link WinBarNC WinBar")
-
-vim.cmd([[
-source $HOME/dotfiles/static/source_norg_ts.lua
-]])
 
 pcall(require, "local") -- load "$HOME/.config/nvim/local.lua" if exists
 require("config.after")
