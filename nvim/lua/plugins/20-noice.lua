9a12d4b37d3ddfbbefd33e85541e5efb950bb963
pysan3 <pysan3@gmail.com>
2024-02-28 21:34:02 +0900

    disable noice lsp-progress

--- nvim/lua/plugins/20-noice.lua
+++ nvim/lua/plugins/20-noice.lua
@@ -122,7 +122,7 @@ M.opts = {
   },
   health = { checker = false },
   lsp = {
-    progress = { enabled = true },
+    progress = { enabled = false },
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
