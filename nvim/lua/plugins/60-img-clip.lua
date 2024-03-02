8cda3ac850d46e0ad1532f3e759a73469ccf0afa
pysan3 <pysan3@gmail.com>
2024-03-02 03:34:50 +0000

    disable img-clip when on ssh

--- nvim/lua/plugins/60-img-clip.lua
+++ nvim/lua/plugins/60-img-clip.lua
@@ -2,6 +2,7 @@ return {
   "HakonHarnes/img-clip.nvim",
   cmd = "PasteImage",
   event = "BufEnter",
+  enabled = not vim.env.SSH_TTY,
   opts = {
     norg = {
       template = [[.image $FILE_PATH$CURSOR]],
