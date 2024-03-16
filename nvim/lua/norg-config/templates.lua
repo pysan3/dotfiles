521ecf36a8a19cc1a7039a2abcd2c17b1226eb8f
pysan3 <pysan3@gmail.com>
2024-03-17 00:25:23 +0900

    update journal template to link correctly with strategy == "nested"

--- nvim/lua/norg-config/templates.lua
+++ nvim/lua/norg-config/templates.lua
@@ -25,4 +25,12 @@ return {
     local ls, m = imports()
     return ls.text_node(m.parse_date(0, os.time(), [[%Y-%m-%d %a %X]])) -- 2023-11-01 Wed 23:48:10
   end,
+  YESTERDAY_OF_FILEPATH = function() -- detect date from filename and return its file path to be used in a link
+    local ls, m = imports()
+    return ls.text_node(m.parse_date(-1, m.file_tree_date(), [[../../%Y/%m/%d]])) -- ../../2020/01/01
+  end,
+  TOMORROW_OF_FILEPATH = function() -- detect date from filename and return its file path to be used in a link
+    local ls, m = imports()
+    return ls.text_node(m.parse_date(1, m.file_tree_date(), [[../../%Y/%m/%d]])) -- <2006-11-01 Wed>
+  end,
 }
