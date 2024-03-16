6dfab8a2b0b8bda2e4361d1235183d9331903c0c
pysan3 <pysan3@gmail.com>
2024-03-17 00:26:19 +0900

    fix description

--- nvim/lua/norg-config/templates.lua
+++ nvim/lua/norg-config/templates.lua
@@ -31,6 +31,6 @@ return {
   end,
   TOMORROW_OF_FILEPATH = function() -- detect date from filename and return its file path to be used in a link
     local ls, m = imports()
-    return ls.text_node(m.parse_date(1, m.file_tree_date(), [[../../%Y/%m/%d]])) -- <2006-11-01 Wed>
+    return ls.text_node(m.parse_date(1, m.file_tree_date(), [[../../%Y/%m/%d]])) -- ../../2020/01/01
   end,
 }
