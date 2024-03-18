baa0f457ad5762851a06f1323b170d001fb77ed7
pysan3 <pysan3@gmail.com>
2024-03-19 05:21:41 +0900

    update notify config

--- nvim/lua/plugins/20-nvim-notify.lua
+++ nvim/lua/plugins/20-nvim-notify.lua
@@ -1,6 +1,6 @@
 return {
   "rcarriga/nvim-notify",
-  event = "VeryLazy",
+  lazy = false,
   opts = {
     stages = "static", -- fade_in_slide_out, fade, slide, static
     render = "default",
@@ -9,6 +9,7 @@ return {
     icons = { ERROR = "", WARN = "", INFO = "", DEBUG = "", TRACE = "✎" },
     level = vim.log.levels.INFO,
     fps = 20,
+    background_colour = "#000000",
     on_open = function(win)
       vim.api.nvim_win_set_config(win, { zindex = 100 })
     end,
