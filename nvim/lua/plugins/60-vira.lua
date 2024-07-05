1d704fdd38739ef3f67e4f34452264c77569db28
pysan3 <pysan3@gmail.com>
2024-07-05 10:45:08 +0900

    add more command to invoke vira

--- nvim/lua/plugins/60-vira.lua
+++ nvim/lua/plugins/60-vira.lua
@@ -1,7 +1,7 @@
 return {
   "n0v1c3/vira",
   build = vim.fn.fnamemodify(vim.g.python3_host_prog, ":h") .. "/pip install --user -r requirements.txt",
-  cmd = { "ViraServers", "ViraIssues", "ViraReport", "ViraFilterEdit" },
+  cmd = { "ViraServers", "ViraIssues", "ViraReport", "ViraFilterEdit", "ViraRefresh" },
   keys = {
     { vim.g.personal_options.prefix.vira .. "f", "ViraFilterEdit" },
     { vim.g.personal_options.prefix.vira .. "r", "ViraReport" },
