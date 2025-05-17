506673be2016c1640f6672a499ffd6009000076e
pysan3 <pysan3@gmail.com>
2025-05-17 22:09:13 +0900

    fix command to update npm packages

--- config/zsh/aliases_rc.zsh
+++ config/zsh/aliases_rc.zsh
@@ -530,7 +530,7 @@ function rebak () {
 alias upgradefp='! command flatpak --help &>/dev/null || ( flatpak update -y && flatpak remove --unused -y )'
 alias upgradepy='pip install --upgrade --user pip pipupgrade && poetry self update && pyenv update && trun python -m pipupgrade --latest --yes'
 alias upgraders='rustup update && trun cargo install-update --all'
-alias upgradejs='npm install -g npm@latest pnpm && pnpm upgrade -g'
+alias upgradejs='npm install -g npm pnpm && pnpm self-update'
 function upgradeall() {
   for lang in fp py rs js; do
     eval "upgrade$lang" \
