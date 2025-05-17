c1d9f504f246cb1cb33b11b1c0fb0687873a073b
pysan3 <pysan3@gmail.com>
2025-05-18 01:20:32 +0900

    set rustaceanvim on_attach to use lsp-base

--- nvim/lua/plugins/80-rustaceanvim.lua
+++ nvim/lua/plugins/80-rustaceanvim.lua
@@ -1,4 +1,14 @@
 return {
   "mrcjkb/rustaceanvim",
   lazy = false,
+  config = function()
+    ---@type rustaceanvim.Opts
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          require("lsp-config.base").on_attach(client, bufnr)
+        end,
+      },
+    }
+  end,
 }
