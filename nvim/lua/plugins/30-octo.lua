c6ac0e38267550077fa786307090dab79bdddd78
pysan3 <pysan3@gmail.com>
2025-06-16 18:49:51 +0900

    add octo keybind to start review of the current branch

--- nvim/lua/plugins/30-octo.lua
+++ nvim/lua/plugins/30-octo.lua
@@ -11,6 +11,7 @@ return {
   keys = {
     { "<Leader>or", "<Cmd>Octo search sort:updated-desc is:open is:pr user-review-requested:@me<CR>" },
     { "<Leader>op", "<Cmd>Octo search is:open is:pr author:@me<CR>" },
+    { "<Leader>os", "<Cmd>Octo review start<CR>" },
   },
   config = function()
     require("octo").setup()
